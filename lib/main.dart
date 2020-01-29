00989e914505a95327e3f1ddf38cd9ac3eafe9eb
dork7 <hamzameh122@gmail.com>
2020-01-29 22:56:30 +0500

    section 1.4

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Center(
-    child: Text("hello flutter", textDirection: TextDirection.ltr),
-  ));
+  runApp(MaterialApp(
+      title: "my flutter app",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("title bar"),
+        ),
+        body: Material(
+          color: Colors.black,
+          child: Center(
+            child: Text(
+              "hello flutter",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(color: Colors.white, fontSize: 55.0),
+            ),
+          ),
+        ),
+      )));
 }
