e5c6d83e8ad50696de12d0802124da7e3e0036b2
dork7 <hamzameh122@gmail.com>
2020-02-01 00:04:51 +0500

    section 2 complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,74 @@
+// import 'dart:js';
+
+ 
 import 'package:flutter/material.dart';
 import './app_screens/home.dart';
 
 void main() {
   runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
+    // debugShowCheckedModeBanner: false,
     title: " Exploring UI widgets ",
-    home: Home(), // this was to call the widgets present in home class
+    // home: Home(), // this was to call the widgets present in home class
     // now to follow tutorial 2.7 i am commenting the home: Home(),
     // home: Scaffold(
     //   appBar: AppBar(
-    //     title: Text("App bar"),
+    //     title: Text("Long List bar"),
     //   ),
     //   body: getlistView(),
     // ),
+////////////////
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Long List bar"),
+      ),
+      body: getListViewLong(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          debugPrint("floating button ");
+        },
+        child: Icon(Icons.add),
+        tooltip: "floating icon",
+      ),
+    ),
   ));
 }
+// snakbar
 
+void showSnackBar(BuildContext context, String item) {
+  var snackBar = SnackBar(
+    content: Text('tapped $item'),
+    action: SnackBarAction(label: "UNDO", onPressed: (){
+      debugPrint("undo button");
+    },),
+  );
+  Scaffold.of(context).showSnackBar(snackBar);
+}
+
+// implementing the long list view
+List<String> getListElements() {
+  var items = List<String>.generate(
+      1000, (counter) => "Item $counter"); // preparing data source
+  return items;
+}
+
+// creating widget to add data into widget
+Widget getListViewLong() {
+  var listItems = getListElements();
+
+  var listView = ListView.builder(itemBuilder: (context, index) {
+    return ListTile(
+      leading: Icon(Icons.arrow_right),
+      title: Text(listItems[index]),
+      onTap: () {
+        debugPrint('${listItems[index]} was tapped');
+        showSnackBar(context , listItems[index]);
+      },
+    );
+  });
+  return listView;
+}
+
+// added a short list
 Widget getlistView() {
   var listView = ListView(
     children: <Widget>[
@@ -31,7 +84,7 @@ Widget getlistView() {
         title: Text("landscape"),
         leading: Icon(Icons.landscape),
         subtitle: Text("Subtitle"),
-                onTap: () {
+        onTap: () {
           debugPrint("landscape");
         },
       ),
